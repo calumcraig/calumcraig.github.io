da3e92c6f7756c20e60ce38163f3ccadfa07da14
calumcraig <calumjcraig@gmail.com>
2026-01-26 22:51:36 +0000

    Add env variables to testing and update GH Actions workflow.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './playwright/tests',
-
   // Adding html reporter to produce test report as standard
   reporter: 'html',
-
   use: {
-    baseURL: 'https://calumcraig.github.io',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'https://calumcraig.github.io',
     headless: false,
   },
 });
